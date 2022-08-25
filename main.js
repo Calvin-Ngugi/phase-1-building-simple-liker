37035f3f09c4526e333e8e7dc657c44a1ced06bc
Calvin <ncalvin67@gmail.com>
2022-08-25 11:41:46 +0300

    Finessed assignment

--- main.js
+++ main.js
@@ -4,16 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let modal = document.getElementById("modal")
 
+let likey = document.querySelectorAll("span.like-glyph")
+
+likey.forEach(heartGlyph =>{
+  heartGlyph.addEventListener("click", () => {
+    mimicServerCall(url="http://mimicServer.example.com", config = {})
+    .then( () => {
+      // debugger
+    if (heartGlyph.innerHTML == EMPTY_HEART) {
+      heartGlyph.innerHTML = FULL_HEART
+      heartGlyph.className = "activated-heart"}
+    else
+      heartGlyph.innerHTML = EMPTY_HEART
+    })
+    .catch( error => {
+      modal.className = ""
+      modal.innerText = error
+      setTimeout( () => {modal.className = "hidden"}, 5000)
+    })
+  })
+  
+})
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
